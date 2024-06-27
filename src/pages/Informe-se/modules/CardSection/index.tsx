7b977b048d7faae932ad80f1463158de9313024d
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-06-27 19:20:12 -0300

    container edit ending

--- src/pages/Informe-se/modules/CardSection/index.tsx
+++ src/pages/Informe-se/modules/CardSection/index.tsx
@@ -6,28 +6,32 @@ import card4Image from "../../../../assets/Card/card4.jpg";
 
 function CardSection() {
   return (
-    <div className="card-grid">
-      <Card
-        title="Entendendo o Tabagismo"
-        image={card1Image}
-        description="O tabagismo é reconhecido como uma doença epidêmica que causa dependência física, psicológica e comportamental."
-      />
-      <Card
-        title="Impacto do Cigarro na Saúde: Doenças e Complicações"
-        image={card2Image}
-        description="O tabagismo causa diversas doenças graves, incluindo problemas respiratórios, cardiovasculares e diferentes tipos de câncer."
-      />
-      <Card
-        title="Benefícios de Parar de Fumar"
-        image={card3Image}
-        description="Quando alguém para de fumar, há uma série de benefícios significativos para a saúde. Confira!"
-      />
-      <Card
-        title="Comparação Entre Métodos de Cessação do Tabagismo"
-        image={card4Image}
-        description="Terapia de reposição de nicotina, medicamentos, acupuntura, hipnose e tratamentos comportamentais. Avalie a eficácia de cada um."
-      />
-    </div>
+    <section className="informe-se">
+      <div className="container">
+        <div className="card-grid">
+          <Card
+            title="Entendendo o Tabagismo"
+            image={card1Image}
+            description="O tabagismo é reconhecido como uma doença epidêmica que causa dependência física, psicológica e comportamental."
+          />
+          <Card
+            title="Impacto do Cigarro na Saúde: Doenças e Complicações"
+            image={card2Image}
+            description="O tabagismo causa diversas doenças graves, incluindo problemas respiratórios, cardiovasculares e diferentes tipos de câncer."
+          />
+          <Card
+            title="Benefícios de Parar de Fumar"
+            image={card3Image}
+            description="Quando alguém para de fumar, há uma série de benefícios significativos para a saúde. Confira!"
+          />
+          <Card
+            title="Comparação Entre Métodos de Cessação do Tabagismo"
+            image={card4Image}
+            description="Terapia de reposição de nicotina, medicamentos, acupuntura, hipnose e tratamentos comportamentais. Avalie a eficácia de cada um."
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
