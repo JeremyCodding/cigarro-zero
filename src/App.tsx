8e8d926a8401d10b36c62793c6a307f4e1fa93e0
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-06-28 04:57:48 -0300

    tela sobre-nos criada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import InformeSe from "./pages/Informe-se";
+import SobreNos from "./pages/Sobre-nos/SobreNos";
 
 function App() {
   const routes = createBrowserRouter([
@@ -12,6 +13,10 @@ function App() {
     {
       path: "/informe-se",
       element: <InformeSe />,
+    },
+    {
+      path: "/Sobre-nos",
+      element: <SobreNos />,
     },
   ]);
   return <RouterProvider router={routes} />;
